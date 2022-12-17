b8b5e02d92782243fe8e04d5d57a60e225655a30
Ruzhikov-Sasha-Maksimovich <109194754+Ruzhikov-Sasha-Maksimovich@users.noreply.github.com>
2022-12-17 12:58:26 +0800

    Update Program2.java

--- Java/Program2.java
+++ Java/Program2.java
@@ -1,15 +1,1 @@
-public class Program2 {
-    public static void main(String[] args) {
-    int a = 6;
-    int b = 3;
-    int sum = a + b;
-    int minus = a - b;
-    int umnozenie = a * b;
-    int delenie = a / b;
-    System.out.print(sum);
-    System.out.print(minus);
-    System.out.print(umnozenie);
-    System.out.print(delenie); 
-    }
 
-}
